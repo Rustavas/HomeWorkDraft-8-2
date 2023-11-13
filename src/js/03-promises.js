0d1a1b3567811a8e55841d98c128d25b814b58ed
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-11-13 21:02:46 +0200

    exercise-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onStartGeneration);
+  
+function onStartGeneration(event){
+  event.preventDefault();
+  // console.log(event.target);
+  // console.log(event.currentTarget);
+  let firstDelay = event.currentTarget.elements.delay.value;
+  let stepDelay = event.currentTarget.elements.step.value;
+  let amountEl = event.currentTarget.elements.amount.value;
+  for(let i=1; i<= amountEl; i+=1){
+    createPromise(i, firstDelay)
+    .then(({position, firstDelay}) => console.log(`✅ Fulfilled promise ${position} in ${firstDelay}ms`))
+    .catch(({position, firstDelay}) => console.log(`❌ Rejected promise ${position} in ${firstDelay}ms`))
+  };
+  firstDelay += stepDelay;
 }
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+        if (shouldResolve) {
+          resolve(position, delay);
+        }   else {
+          reject(position, delay);
+        }; 
+      },delay);
+    })
+  };
+  
+  
+
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
